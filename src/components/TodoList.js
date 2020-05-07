3b428f69cad060f89f189e857ab75351ab8f5c9b
TheTrabin <beyondtears@gmail.com>
2020-05-06 20:57:05 -0700

    MVP complete. Now comes the styling

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+import Todo from "./Todo";
+
+const TodoList = props => {
+  return (
+    <div className="todo-list">
+      {props.todo.map(item => (
+        <Todo
+          toggleTaskComplete={props.toggleTaskComplete}
+          key={item.id}
+          item={item}
+        />
+      ))}
+      <button className="clear-btn" onClick={props.clearComplete}>
+        Clear Complete
+      </button>
+    </div>
+  );
+};
+
+export default TodoList;
